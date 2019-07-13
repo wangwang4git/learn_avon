fd953866c8279c4d5587612848210fda74459775
victorwwang <victorwwang@tencent.com>
2019-07-13 16:43:21 +0800

    Initial Commit

---
+++ learn_avon/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learn_avon
+//
+//  Created by wangwang on 2019/7/13.
+//  Copyright © 2019 wangwang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
